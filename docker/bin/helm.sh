fb19286e0e8dae27eb837bdf4b72dadcd499ceba
Jeff Tunessen <jeff.tunessen@gmail.com>
2022-01-29 20:24:30 +0100

    dev: dockerception helm

--- docker/bin/helm.sh
+++ docker/bin/helm.sh
@@ -2,28 +2,35 @@
 
 set -e
 
-: "${REDO_HELM:=alpine/helm:latest}"
-: "${REDO_HELM_EXEC:=}"
+: "${REDO_HELM_IMG:=alpine/helm:latest}"
+: "${REDO_HELM_CMD:=}"
 : "${REDO_HELM_DIR:=.}"
 
-ARGUMENTS=""
+_ARGS_=()
 
 while [ $# -gt 0 ]; do
   case "$1" in
-    --REDO_HELM=*)
-      REDO_HELM="${1#*=}"
+    --REDO_HELM_IMG=*)
+      REDO_HELM_IMG="${1#*=}"
       ;;
-    --REDO_HELM_EXEC=*)
-      REDO_HELM_EXEC="${1#*=}"
+    --REDO_HELM_CMD=*)
+      REDO_HELM_CMD="${1#*=}"
       ;;
     --REDO_HELM_DIR=*)
       REDO_HELM_DIR="${1#*=}"
       ;;
     *)
-      ARGUMENTS="$ARGUMENTS $1"
+      _ARGS_+=($1)
   esac
   shift
 done
 
-dockerception-pull $REDO_HELM
-dockerception-run $REDO_HELM_DIR /app "--volume $(pwd)/.helm/cache:/root/.cache/helm" "--volume $(pwd)/.helm/config:/root/.config/helm" "--volume $(pwd)/.helm/data:/root/.local/share/helm" $REDO_HELM $REDO_HELM_EXEC $ARGUMENTS
+_ARGS_RUN_=()
+_ARGS_RUN_+=("--volume $(pwd)/$REDO_HELM_DIR/.helm/cache:/root/.cache/helm")
+_ARGS_RUN_+=("--volume $(pwd)/$REDO_HELM_DIR/.helm/config:/root/.config/helm")
+_ARGS_RUN_+=("$REDO_HELM_IMG")
+_ARGS_RUN_+=("$REDO_HELM_CMD")
+_ARGS_RUN_+=("${_ARGS_[*]}")
+
+dockerception-pull "$REDO_HELM_IMG"
+dockerception-run "$REDO_HELM_DIR" "/app" "${_ARGS_RUN_[*]}"
